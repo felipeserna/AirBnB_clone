5eb9ef97ae13220801ac0bca851beb490de46fd1
Cholado <sntgmtnz@gmail.com>
2020-06-30 10:24:24 -0500

    class verifycation added & data values asignation set

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+l_c = {"Amenity": Amenity, "BaseModel": BaseModel,
+       "City": City, "Place": Place,
+       "Review": Review, "State": State, "User": User
+}
 
 
 class FileStorage:
@@ -51,11 +55,14 @@ class FileStorage:
     def reload(self):
         """
         deserializes the JSON file to __objects
+        i = items , objects loaded
+        k = keys , attribute names loaded
+        there is a double pointer to asign values to those attributes
         """
         try:
             with open(self.__file_path, 'r') as my_file:
-                item = json.load(my_file)
-            for key in item:
-                    self.__objects[key] = BaseModel(**item[key])
+                i = json.load(my_file)
+            for k in i:
+                    self.__objects[k] = l_C[i[k]["__class__"]](**i[k])
         except:
             pass
